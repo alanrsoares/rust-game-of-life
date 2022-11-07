704d3c39626510e88fe7aa376ae02eb75f36ab90
Alan Soares <alanrsoars@gmail.com>
2022-11-07 14:13:35 +1300

    chore: cleaner loop

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use game_of_life::Grid;
 
 fn main() {
-    const GENERATIONS: usize = 60;
-    const DELAY: u64 = 160;
+    const MAX_GENERATIONS: usize = 100;
+    const FRAME_DELAY: u64 = (1000 / 60) * 8;
+
+    let mut current_generation = 0;
 
     let mut new_grid = Grid::random(24, 24);
 
-    for _ in 0..GENERATIONS {
+    'game_loop: loop {
         new_grid.next_state().render();
 
+        println!("Generation: {}", current_generation);
+        println!("\nhit ctrl-c to exit\n");
+
+        if current_generation == MAX_GENERATIONS {
+            break 'game_loop;
+        }
+        current_generation += 1;
         // delay printing to console
-        std::thread::sleep(std::time::Duration::from_millis(DELAY));
+        std::thread::sleep(std::time::Duration::from_millis(FRAME_DELAY));
     }
 }
