8fbdd85f63d1e3db98319e7740c84d9404a16765
Alan Soares <alanrsoars@gmail.com>
2022-11-08 09:35:05 +1300

    chore: extract constansts from main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use game_of_life::Game;
 use game_of_life::Grid;
 
-fn main() {
-    const GRID_SIZE: i32 = 20;
-    const MAX_GENERATIONS: usize = 100;
-    const FRAME_DELAY: u64 = (1000 / 60) * 6;
+const GRID_SIZE: i32 = 20;
+const MAX_GENERATIONS: usize = 100;
+const FRAME_DELAY: u64 = (1000 / 60) * 6;
 
+fn main() {
     let mut game = Game::new(
         Grid::random(GRID_SIZE, GRID_SIZE),
         MAX_GENERATIONS,
